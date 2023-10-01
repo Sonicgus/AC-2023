245dc3d462cb2a85409ab4c8591687fd2d483e6d
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-10-01 22:11:01 +0100

    Update 2.asm

--- LAB3/2.asm
+++ LAB3/2.asm
@@ -6,19 +6,33 @@ table:  .byte 63, 6, 91, 79, 102, 109, 125, 7, 127, 103, 119 , 124, 57, 94, 121,
 
 main:
     li $t0, 0xFFFF0010
-    li $v0, 0xFFFF0012
-    li $v1, 0xFFFF0014
-    la $t1, table
-    li $t2, 0x0
+    li $t1, 0xFFFF0012
+    li $t2, 0xFFFF0014
+    la $t3, table
+    
+    li $v0, 3
         
+    start:
+    li $t4, 1
+    
     for:
-    sb $zero, ($v0)
     
-    lbu $t2, 0($v1)
-    andi $t2, $t2, 0x0F
+    sb $t4, ($t1)
     
-    add $t2, $t2, $t1
-    lbu $t2, 0($t2)
-    sb $t2, 0($t0)
-        
-    bne $zero 1, for
+    lb $t5, ($t2)
+    
+    bne $t5, $zero, draw
+    
+    sll $t4, $t4, 1
+    
+    bne $t4, 16, for
+    
+    sb $zero, ($t0)
+    
+    j start
+    
+    draw:
+    
+    sb $v0, ($t0)
+    
+    j start
