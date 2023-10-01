d96355ba3b57dd28270f5cee4035c6d9edaede82
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-10-01 14:36:07 +0100

    Create c.asm

---
+++ LAB3/c.asm
@@ -0,0 +1,20 @@
+.data
+table:  .byte 63, 6, 91, 79, 102, 109, 125, 7, 127, 103, 119 , 124, 57, 94, 121, 113
+
+.text
+.globl main
+
+main:
+    li $t0, 0xFFFF0010
+    li $t1, 0x0
+    la $t4, table
+    li $t8 0
+    
+    for:
+    lbu $t2, 0($t4)
+
+    sb $t2, 0($t0)
+    
+    add $t4, $t4, 1
+    add $t8, $t8, 1
+    bne $t8, 16, for
