66c76a0acba5d30c75f80a1dbb87f5bd4b31acac
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-10-02 15:18:49 +0100

    está invertido kekw

--- LAB3/2.asm
+++ LAB3/2.asm
@@ -5,36 +5,51 @@ table:  .byte 63, 6, 91, 79, 102, 109, 125, 7, 127, 103, 119 , 124, 57, 94, 121,
 .globl main
 
 main:
-    li $t0, 0xFFFF0010
-    li $t1, 0xFFFF0012
-    li $t2, 0xFFFF0014
-    la $t3, table
+    li $a0, 0xFFFF0010 # endereço do display direito
+    li $a1, 0xFFFF0012 # inserir bit a 1 no em qual das linhas queremos ler
+    li $a2, 0xFFFF0014 # ler qual linha e coluna na qual foi obtida
+    la $a3, table # endereço de memoria da tabela de simbolos
         
     start:
-    li $t4, 1
+    li $t0, 1 #começar na primeira linha index 0
     
     for:
-    
-    sb $t4, ($t1)
-    
-    lh $t5, ($t2)
-    
-    bne $t5, $zero, draw
-    
-    sll $t4, $t4, 1
-    
-    bne $t4, 16, for
-    
-    sb $zero, ($t0)
-    
-    j start
-    
+    sb $t0, ($a1) # mediar a linha t0
+    lbu $t1, ($a2) # guardar informaçao de teclas pressionadas
+    bne $t1, $zero, draw # encontrou uma tecla pressionada
+    sll $t0, $t0, 1 # avançar para a proxima linha
+    bne $t0, 16, for # enquanto nao percorremos todas as linhas, continuar o ciclo
+    sb $zero, ($a0) # caso nao tenha sido pressionado nenhuma tecla, desligar o ecra
+    j start # verificar se alguma tecla foi pressionada desde o começo
     draw:
-        
-    add $v0, $v0, $t3
+    #começar pelas linhas
+    andi $v0,$t1, 0xF0
     
-    lb $v0, ($v0)
+    srl $v0, $v0,4
     
-    sb $v0, ($t0)
+    li $t2, 0 #contar linhas
+    contarl:
+    beq $v0, 1 , terminarcl
+    srl $v0, $v0, 1
+    addi $t2, $t2, 1
+    j contarl
+    terminarcl:
+    mulu $t2,$t2,4
+    
+    andi $v0,$t1, 0x0F
+    
+    contarc:#contar colunas
+    beq $v0, 1 , terminarc
+    srl $v0, $v0, 1
+    addi $t2, $t2, 1
+    j contarc
+    terminarc:    
+    add $t2, $t2, $a3
+    
+    lbu $t2, ($t2)
+    
+    sb $t2, ($a0)
     
     j start
+
+    
