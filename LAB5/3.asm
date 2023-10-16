00fd4f98def05c412e33c6741720cb2ebf090545
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-10-16 17:18:33 +0100

    Create 3.asm

---
+++ LAB5/3.asm
@@ -0,0 +1,38 @@
+	.data
+str:	.asciiz "Uma Frase!"
+	.text
+	.globl main
+main:
+	la $a0, str
+	li $a1, 'c'
+	
+	li  $v0, 4
+    	syscall
+	
+	jal codificaString
+	
+	li  $v0, 4
+    	syscall
+    	
+    	jal codificaString
+	
+	li  $v0, 4
+    	syscall
+	
+	li $v0, 10
+    	syscall
+
+codificaString:
+	move $s0, $a0 #copia
+for:
+	lbu $s1, 0($s0)
+	beq $s1, 0, sair
+	
+	xor $s1, $s1, $a1
+	sb $s1, ($s0)
+	
+salta:
+	addi $s0, $s0, 1
+	j for
+sair:
+	jr $ra
