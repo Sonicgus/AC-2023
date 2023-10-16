a7bed2fc5366eca9aabb00cdae3ea20afdb9830d
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-10-16 17:11:47 +0100

    Create 2iii.asm

---
+++ LAB5/2iii.asm
@@ -0,0 +1,31 @@
+	.data
+str:	.asciiz "Uma Frase!"
+	.text
+	.globl main
+main:
+	la $a0, str
+	
+	jal funcao
+	
+	li  $v0, 4
+    	syscall
+	
+	li $v0, 10
+    	syscall
+
+funcao:
+	move $s0, $a0 #copia
+for:
+	lbu $s1, 0($s0)
+	beq $s1, 0, sair
+	blt $s1, 0x61, salta
+	bgt $s1, 0x7A, salta
+	
+	addi $s1, $s1, -32
+	sb $s1, ($s0)
+	
+salta:
+	addi $s0, $s0, 1
+	j for
+sair:
+	jr $ra
